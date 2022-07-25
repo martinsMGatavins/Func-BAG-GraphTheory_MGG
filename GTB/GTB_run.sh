192a527684f8fd88386ad7b3b077087bd723fa6c
martinsMGatavins <91304281+martinsMGatavins@users.noreply.github.com>
2022-07-25 14:38:09 -0500

    Update GTB_run.sh

--- GTB/GTB_run.sh
+++ GTB/GTB_run.sh
@@ -11,18 +11,24 @@
 #SBATCH --mail-user=gatavins@wustl.edu
 #SBATCH --mail-type=END
 
+# set dirs for input data (already split into train and test)
+# add line to parse data (or do it on local machine)
+# Rscript splitting.R -> make sure to read data to train_test_tempdata (or alike)
 projectdir=/home/gatavins/brain_age
 inputdir=${projectdir}/data/train_test_tempdata
 
+# list of models to train - adjust number of array jobs if you change # of models
 declare -a ARRAY=("alff" "reho" "intrafc" "interfc" "modul"
                   "centr" "eff" "part" "clust" "bc" "str")
 i=`expr ${SLURM_ARRAY_TASK_ID} - 1`
 SUB=${ARRAY[$i]}
 
-category=${SUB}_firstround
+# makes output directory
+category=${SUB}
 mkdir ${projectdir}/outputs/${category}
 outputdir=${projectdir}/outputs/${category}
 
+# run command
 module load python
 python GTB_nested.py \
   "${inputdir}/${SUB}_train.csv" \
